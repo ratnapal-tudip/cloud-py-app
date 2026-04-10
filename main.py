c60060597e96904e3200bcbb34e553f565e173d6
Ratnapal Shende <ratnapal.shende@tudip.com>
2026-04-10 19:35:12 +0530

    add mysql connector

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
-import pymysql
+import mysql.connector
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-host=os.environ.get("host")
-user=os.environ.get("user")
-password=os.environ.get("password")
-database=os.environ.get("database")
+host = os.environ.get("host")
+user = os.environ.get("user")
+password = os.environ.get("password")
+database = os.environ.get("database")
 
 app = FastAPI()
 
 def get_connection():
-    return pymysql.connect(
+    return mysql.connector.connect(
         host=host,
         user=user,
         password=password,
         database=database,
-        cursorclass=pymysql.cursors.DictCursor
+        connection_timeout=10
     )
 
 @app.get("/users")
 def get_users():
     conn = get_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)  # 👈 returns dict like PyMySQL
 
     cursor.execute("SELECT * FROM Users")
     users = cursor.fetchall()
